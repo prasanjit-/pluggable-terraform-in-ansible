5e76714db33ce74b4b909f0ee849659c6abc2f5c
Prasanjit Singh <mailprasanjit@gmail.com>
2018-11-09 00:08:59 +0400

    Update backend.tf

--- terraform-scripts/providers/aws/backend.tf
+++ terraform-scripts/providers/aws/backend.tf
@@ -2,10 +2,10 @@ terraform {
 
   backend "s3" {
     profile = "poc"
-    region  = "eu-west-1"
+    region  = "${var.aws_region}"
     bucket          = "containers-terraform-state-storage"
     dynamodb_table  = "terraform-state-lock"
-    key     = "service/ansible/terraform.tfstate"
+    key     = "service/ansible/terraform_planned.tfstate"
     encrypt = true
   }
 }
